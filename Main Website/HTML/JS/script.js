a729688c5a4f073b7ae1231501216275fdc634ab
Sunidhi Mistry <63811206+sunidhimm235@users.noreply.github.com>
2020-08-21 13:50:22 -0400

    UPDATE

--- Main Website/HTML/JS/script.js
+++ Main Website/HTML/JS/script.js
@@ -26,6 +26,15 @@ function ready() {
     document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
 }
 
+function purchaseClicked() {
+    alert('Thank you for your purchase')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
 function removeCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
