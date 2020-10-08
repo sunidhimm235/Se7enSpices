400cbfaec6e0e524beb6a06a48b84722fe007647
Sunidhi Mistry <63811206+sunidhimm235@users.noreply.github.com>
2020-10-08 00:14:36 -0400

    Completed

--- Main Website/HTML/JS/script.js
+++ Main Website/HTML/JS/script.js
@@ -1,3 +1,6 @@
+//Please regard THIS PART of the CODE as it is not used right now (THIS CODE is saved for later use.)
+//Please scroll down to see the staring of the MAIN CODE.
+
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready)
 } else {
@@ -23,7 +26,7 @@ function ready() {
         button.addEventListener('click', addToCartClicked)
     }
 
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', onFormSubmit)
 }
 
 function purchaseClicked() {
@@ -78,7 +81,7 @@ function addItemToCart(title, price, imageSrc) {
         <span class="cart-price cart-column">${price}</span>
         <div class="cart-quantity cart-column">
             <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">REMOVE</button>
+            <button class="btn btn-danger" type="button" onclick="deleteValue('')">REMOVE</button>
         </div>`
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
@@ -101,3 +104,81 @@ function updateCartTotal() {
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = '₹' + total
 }
+
+
+
+function onFormSubmit() {
+    var formData = readFormData();
+    insertNewRecord(formData);
+
+    alert('Thank you for your purchase')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
+function readFormData() {
+
+    var formData = {};
+
+    formData["fullName"] = document.getElementById('fNameinput').value
+    formData["lastName"] = document.getElementById('lNameinput').value
+    formData["address"] = document.getElementById('deliveryAddressinput').value
+    formData["email"] = document.getElementById('deliveryEmailinput').value
+    formData["phoneNumber"] = document.getElementById('deliveryNumberinput').value
+    formData["order"] = document.getElementsByClassName('cart-items')[0].innerText
+    formData["quantity"] = document.getElementsByClassName('cart-quantity-input')[0].value
+    formData["total"] = document.getElementsByClassName('cart-total-price')[0].innerText
+
+    return formData;
+
+}
+
+function insertNewRecord(data) {
+
+    var table = document.getElementById('orderDetails').getElementsByTagName('tbody')[0];
+
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.lastName;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.address;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.email;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.phoneNumber;
+    cell6 = newRow.insertCell(5);
+    cell6.innerHTML = data.order;
+    cell7 = newRow.insertCell(6);
+    cell7.innerHTML = data.quantity;
+    cell8 = newRow.insertCell(7);
+    cell8.innerHTML = data.total;
+}
+
+//main Code STARTS from HERE.
+
+//This part is related to the menu TAB SECTION. 
+var tabs = document.querySelectorAll(".tabs ul li");
+var tab_wraps = document.querySelectorAll(".tabWrap");
+
+tabs.forEach(function(tab, tab_index) {
+    tab.addEventListener("click", function() {
+        tabs.forEach(function(tab) {
+            tab.classList.remove("AcTive");
+        })
+        tab.classList.add("AcTive");
+
+        tab_wraps.forEach(function(content, content_index) {
+            if (content_index == tab_index) {
+                content.style.display = "block";
+            } else {
+                content.style.display = "none";
+            }
+        })
+
+    })
+})
